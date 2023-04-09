b7ea041c9a079289eea6ffcd2979090cd3243687
C1325334 Elvis Thermo Carvalho Miranda <c1325334@interno.bb.com.br>
2023-04-09 17:59:58 -0300

    ajust error to build

--- src/pages/components/Skills/index.tsx
+++ src/pages/components/Skills/index.tsx
@@ -1,14 +1,102 @@
 import React from "react";
 import Image from "next/image";
-import  cards  from "./data";
 import { useTranslation } from "react-i18next";
+interface Jobs{
+   cards: Cards[];
+}
+
+interface Cards{ 
+  title:string;
+  url: string;
+}
 
 const Skills = () => {
   const { t } = useTranslation();
-
+  const cardsjobs:Cards[] =   [
+    {
+      title: "HTML5",
+      url: "/assets/images/html.png",
+    },
+    {
+      title: "CSS3",
+      url: "/assets/images/css.png",
+    },
+    {
+      title: "JavaScript",
+      url: "/assets/images/js.png",
+    },
+    {
+      title: "Java",
+      url: "/assets/images/java.png",
+    },
+    {
+      title: "Typescript",
+      url: "/assets/images/ts.png",
+    },
+    {
+      title: "Node.js",
+      url: "/assets/images/node.png",
+    },
+    {
+      title: "Nest.js",
+      url: "/assets/images/NestJS.png",
+    },
+    {
+      title: "NextJS",
+      url: "/assets/images/nextjs.png",
+    },
+    {
+      title: "Quarkus",
+      url: "/assets/images/quarkuscom.png",
+    },
+    {
+      title: "Spring Boot",
+      url: "/assets/images/spring-logo.png",
+    },
+    {
+      title: "Go",
+      url: "/assets/images/golang.png",
+    },
+    {
+      title: "C#",
+      url: "/assets/images/csharp.png",
+    },
+    {
+      title: "Git",
+      url: "/assets/images/git.png",
+    },
+    {
+      title: "Docker",
+      url: "/assets/images/docker.png",
+    },
+    {
+      title: "D3.js",
+      url: "/assets/images/d3js.png",
+    },
+    {
+      title: "Karma.js",
+      url: "/assets/images/karma.png",
+    },
+    {
+      title: "ReactJs",
+      url: "/assets/images/reactjs.png",
+    },
+    {
+      title: "Angular",
+      url: "/assets/images/angular.png",
+    },
+    {
+      title: "Electron",
+      url: "/assets/images/electron.png",
+    },
+    {
+      title: "Kubernets",
+      url: "/assets/images/kubernets.png",
+    },
+  ]
   return (
     <div className="cards">
-      {cards.cards.map((card, index) => (
+      {cardsjobs.map((card, index) => (
         <div className="card animate__animated animate__bounceInLeft" key={index}>
           <Image 
           width={100}
