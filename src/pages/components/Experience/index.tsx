8a410541ba2306ee778e4b30acec3e5b2b346760
C1325334 Elvis Thermo Carvalho Miranda <c1325334@interno.bb.com.br>
2023-04-09 16:56:22 -0300

    remove erro to build

--- src/pages/components/Experience/index.tsx
+++ src/pages/components/Experience/index.tsx
@@ -1,13 +1,22 @@
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
+interface Job {
+  title: string;
+  subtitle: string;
+  date: string;
+  description:string;
+
+}
 
 export function Experience() {
   const { t } = useTranslation();
 
+  const jobs = t("jobs", { returnObjects: true }) as Job[];
+
   return (
     <div className="cards">
-      {t("jobs", { returnObjects: true }).map((job,index) => (
-        <div className="card" key={index}>
+      {jobs.map((job,index) => (
+        <div className="card" key={`${job}-index`}>
           <h3>{t(`${job.title}`)}</h3>
           <h3>{t(`${job.subtitle}`)}</h3>
           <div className="wrapper"></div>
